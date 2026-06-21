7fbeba6fe2753a948f974141b779f342dd567603
Gubba Srinivas <srinivasgubba2006@gmail.com>
2026-06-21 17:39:38 +0530

    updated main class

--- frist-spring-app/src/main/java/com/fristspringboot/example/frist_spring_app/FristSpringAppApplication.java
+++ frist-spring-app/src/main/java/com/fristspringboot/example/frist_spring_app/FristSpringAppApplication.java
@@ -9,7 +9,7 @@ public class FristSpringAppApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(FristSpringAppApplication.class, args);
-		System.out.println("hello");
+		System.out.println("hello world");
 	}
 
 }
