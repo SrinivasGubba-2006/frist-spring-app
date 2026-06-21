8360351a3bab1033bf1378d939633469535d86a7
Gubba Srinivas <srinivasgubba2006@gmail.com>
2026-06-21 17:08:12 +0530

    creating frist spring boot project and running

---
+++ frist-spring-app/src/main/java/com/fristspringboot/example/frist_spring_app/FristSpringAppApplication.java
@@ -0,0 +1,15 @@
+package com.fristspringboot.example.frist_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristSpringAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FristSpringAppApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
